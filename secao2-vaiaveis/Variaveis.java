276b4335ac83b6db8df3ad24208fdbbc755b0692
Clairtonlm <clairtonlm@gmail.com>
2024-12-10 15:12:54 -0300

    Atualizando

---
+++ secao2-vaiaveis/Variaveis.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Variaveis {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite seu nome");
+        String nome = scanner.nextLine();
+
+        System.out.println("Nome digitado:" + nome);
+
+
+
+
+
+
+        scanner.close();
+    }
+}
