d020628ff394c40e583a1b3d2bccbc85bd085089
Clairtonlm <clairtonlm@gmail.com>
2024-12-28 21:14:42 -0300

    Adiciona a classe Funcoes com métodos de saudação, soma e dobrar

---
+++ secao6/Funcoes.java
@@ -0,0 +1,37 @@
+package secao6;
+
+public class Funcoes {
+    public static void main(String[] args) {
+        saudacao();
+        // 2- paramentros
+        soma(5, 5);
+        soma(8, 12);
+        //3-Returm
+        saudar("Maria");
+
+        int resultado = dobrar(5);
+        System.out.println("O resultado é: " + resultado);
+
+        int numero = 10;
+        int resultado2 = dobrar(numero);
+        System.out.println("O resultado é: " + resultado2);
+
+
+
+    }
+    // funçoes em Java
+    public static void saudacao() {
+        System.out.println("Olá, tudo bem? Bem-vindo ao Java!");
+    }
+    public static void soma(int a, int b){
+        int resultado = a + b;
+        System.out.println(" O resultado da soma é: " + resultado);
+    }
+    public static void saudar(String nome){
+        System.out.println("Olá, " + nome + " tudo bem?");
+    }
+    public static int dobrar(int n){
+        return n * 2;
+    }
+
+}
