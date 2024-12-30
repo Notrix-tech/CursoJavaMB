37c7d97cb9eabec5a58991a455120fa24b5bad68
Clairtonlm <clairtonlm@gmail.com>
2024-12-30 20:48:21 -0300

    Add VerificarIdade class to validate user age input and control access

---
+++ secao6/VerificarIdade.java
@@ -0,0 +1,31 @@
+package secao6;
+
+import java.util.Scanner;
+
+public class VerificarIdade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int idade;
+
+        do {
+            System.out.println("Digite sua idade: ");
+            idade = scanner.nextInt();
+            scanner.nextLine(); // Limpa o buffer
+
+            if (idade <=0) {
+                System.out.println("Idade inválida! Por favor, digite um número positivo.");
+                continue;
+            } 
+            
+            if (idade < 18) {
+                System.out.println("Acesso negado - Menor de idade");
+            } else {
+                System.out.println("Acesso permitido - Maior de idade");
+            }
+            break; // Sai do loop após uma entrada válida
+            
+        } while (true);
+
+        scanner.close();
+    }
+}
