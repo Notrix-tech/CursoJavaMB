442cddfadf3b5583bdd449009f7478a4114a8701
Clairtonlm <clairtonlm@gmail.com>
2024-12-30 20:49:10 -0300

    Add documentation to main method in VerificarIdade class to clarify age verification process

--- secao6/VerificarIdade.java
+++ secao6/VerificarIdade.java
@@ -3,6 +3,11 @@ package secao6;
 import java.util.Scanner;
 
 public class VerificarIdade {
+    /**
+     * Verifica se o usuário é maior de idade
+     * 
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int idade;
