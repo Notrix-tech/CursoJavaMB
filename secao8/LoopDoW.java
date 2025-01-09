8be72aec0621faaccd17c8815a2d7928899a1421
Clairtonlm <clairtonlm@gmail.com>
2025-01-08 22:37:29 -0300

    Add LoopDoW class demonstrating Do While loop in Java

---
+++ secao8/LoopDoW.java
@@ -0,0 +1,13 @@
+package secao8;
+
+public class LoopDoW {
+    public static void main(String[] args) {
+        // - Do While loop
+       int i = 10;
+       do{
+        System.out.println(" O valor de i é: "+i);
+        i--;
+       }while(i > 0);
+    }
+    
+}
