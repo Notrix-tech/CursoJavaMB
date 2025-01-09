e42cd7a0c6c31a74acbb008a06c5befaa322daf1
Clairtonlm <clairtonlm@gmail.com>
2025-01-08 22:52:57 -0300

    atz

---
+++ secao8-ER/Loop.java
@@ -0,0 +1,10 @@
+public class Loop {
+    public static void main(String[] args) {
+
+        // - For loop (for each)    
+        // loop vai rodar de 1 a 5
+        for (int i = 1; i <= 5; i++){
+            System.out.println("O valor de i é : " + i);
+        }
+    }
+}
