97bd793914874b32afc6cdefd385e493170173d0
Clairtonlm <clairtonlm@gmail.com>
2025-01-17 16:45:25 -0300

    Add Listas class demonstrating array syntax and operations in Java

--- secao2-vaiaveis/Exercicio1.java
+++ secao2-vaiaveis/Exercicio1.java
@@ -18,3 +18,4 @@ public class Exercicio1 {
         System.out.println(soma);
     }
 }
+
---
+++ secao9rreis/Listas.java
@@ -0,0 +1,50 @@
+package secao9rreis;
+
+public class Listas {
+    public static void main(String[] args) {
+        // 1 - Sintaxe de Arreis
+        int [] numeros = {1, 2, 3, 4, 5, 10};
+        System.out.println("O valor do primeiro elemento é : " + numeros[0]);
+
+        // Tamaho fixo, só que vazio
+        String [] nomes = new String [3];
+        nomes[0] = "Maria";
+        nomes[1] = "João";
+        nomes[2] = "José";
+        System.out.println("O valor do segundo elemento é : " + nomes[2]);
+
+        //2 loops em arrays
+        //somar todos os elementos de um array
+        // acessar os elemntos e condensar a soma deles em uma variavel
+        int soma = 0;
+        for (int i = 0; i < numeros.length; i++) {
+            soma += numeros[i];
+        }
+        System.out.println("A soma dos elementos do array é : " + soma);
+
+        // for each
+        for(String qualquerCoisa : nomes){
+            System.out.println(" A cada passagem é um nome diferente " + qualquerCoisa);
+        }
+
+        //3 - encontrar o maior e o menor valor de um array
+        int[] valores = {1, 2, 3, 4, 5, 6, 7, 30, 9};
+        int maior = valores[0];
+        int j = 0;
+        while (j < valores.length) {
+            if(valores[j] > maior){
+                maior = valores[j];
+            }
+            j++;
+        }
+        System.out.println("O maior valor do array é : " + maior);
+
+        for(int num: numeros){
+            if(num % 2 == 0){
+                System.out.println("O valor é par " + num);
+            }else if(num % 2 != 0){
+                System.out.println("O valor é impar " + num);
+            }
+        }
+    }
+}
