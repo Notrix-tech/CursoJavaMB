a2af88e4ef0eb1b12b1af7be778f9287c616e263
Clairtonlm <clairtonlm@gmail.com>
2025-01-22 14:18:49 -0300

    Novos elementos no Array

--- secao9rreis/MaiorNum.java
+++ secao9rreis/MaiorNum.java
@@ -1,7 +1,13 @@
 package secao9rreis;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class MaiorNum {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
         int[] valores = {1, 2, 3, 4, 5, 6, 7, 30, 9};
         int limite = 100;
         for (int i = 0; i < valores.length; i++){
@@ -11,6 +17,29 @@ public class MaiorNum {
             System.out.println("Não ha valor ");
         }
 
+        // encontrat Linguagem
+        String[] palavras = {"Java", "PHP", "Python", "C#", "C++", "Ruby"};
+
+        System.out.println("Digite uma linguagem de programação: ");
+        String linguagem = sc.nextLine();
+
+        for (String palavra : palavras){
+            if(palavra.equals(linguagem)){
+                System.out.println("Linguagem encontrada " + linguagem);
+                
+            }
+        }
+
+        // metodo to estring
+        String dadosNumericos = java.util.Arrays.toString(valores);
+        System.out.println(dadosNumericos);
+
+        String nomesLinguagem = Arrays.toString(palavras);
+        System.out.println(nomesLinguagem);
+
+        // Adcionando novos itens a Array
+        
+       
+        sc.close();
     }
-    
 }
