30d0a2d8af53b703e1e53d8f5252ed4a2f7e2e8c
Clairton Lima <108296651+Clairtonlm@users.noreply.github.com>
2025-02-25 07:45:51 -0300

    Create Banco.java

---
+++ secao13/Banco.java
@@ -0,0 +1,13 @@
+public class Banco{
+  private double saldo;
+
+public Banco(double saldo){
+  this.saldo = saldo;
+}
+  public void sacar(double valor){
+    // exceção aqui
+  }
+  saldo = saldo - valor;
+sout(" Saque de" + valor + " realizado com sucesso ! ")
+
+  }
