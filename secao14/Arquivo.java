6a4740aaa0be3eeb2448e79bfb0c0899703296a6
Clairtonlm <clairtonlm@gmail.com>
2025-03-29 15:02:51 -0300

    Implement file writing and object serialization in Arquivo class; add Pessoa class for serialization

--- secao14/Arquivo.java
+++ secao14/Arquivo.java
@@ -38,8 +38,52 @@ public class Arquivo {
            System.out.println("Erro ao ler o arquivo" + e.getMessage());
         }
 
+        // 2 - escrevendo em arquivos
+        //se o arquivo nao existe ele é automaticamente criado pelo Java
+        try (FileWriter writer = new FileWriter(currentDir + "saida.pdf")) {
 
+            writer.write("escrevendo em arquivo \n");
+            writer.write("escrevendo em arquivo 2");
+            
+        } catch (Exception e) {
+            System.out.println("Erro ao escrever no arquivo" + e.getMessage());
+        }
 
+        //Esse é mais moderno
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(currentDir + "saida2.txt", true))) {
 
+            writer.write("Escrenvendo em arquivo com BufferedReader");
+            writer.newLine();
+            writer.write("Escrenvendo a linha dois sem usar o quebra linha com barra n");
+            
+        } catch (Exception e) {
+            System.out.println("Erro ao escrever no arquivo" + e.getMessage());
+        }
+
+        System.out.println("-----------------Serializable--------------------");
+
+        //3 serialização de objetos
+        Pessoa pessoa = new Pessoa("ClairtonLima", 45);
+        System.out.println(pessoa.getNome());
+        // - SERIALIZAÇÃO
+        // tem extensao .ser
+        
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(currentDir + "pessoa.ser"))) {
+            oos.writeObject(pessoa);
+            System.out.println("Objeto serializado com sucesso");
+            
+        } catch (Exception e) {
+            System.out.println("Erro ao serializar objeto" + e.getMessage());
+        }
+        //4 Deserialização do obejto
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(currentDir + "pessoa.ser"))) {
+
+            Pessoa pessoa2 = (Pessoa) ois.readObject();
+            System.out.println("Nome: " + pessoa2.getNome());
+            System.out.println("Idade: " + pessoa2.getIdade());
+            
+        } catch (Exception e) {
+            System.out.println("Erro ao desserializar objeto" + e.getMessage());
+        }
     }//fim main
 }
