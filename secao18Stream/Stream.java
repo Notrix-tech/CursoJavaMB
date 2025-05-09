aeab87abdc7ea2e30b85b84f3abc09348d57084a
Clairton Lima <108296651+Clairtonlm@users.noreply.github.com>
2025-05-09 07:25:20 -0300

    Trbalhando com REDUCE

--- secao18Stream/Stream.java
+++ secao18Stream/Stream.java
@@ -26,6 +26,13 @@ public class Stream {
      */
     public static void main(String[] args) {
 
+        Set<String> nomes = new HashSet<>();
+         nomes.add("João");
+         nomes.add("Pedro");
+         nomes.add("Ana");
+         nomes.add("carvalho ");
+         nomes.add("Willian");
+
         List<Integer> numeros = new LinkedList<>();
         numeros.add(10);
         numeros.add(20);
@@ -41,5 +48,61 @@ public class Stream {
         System.out.println(numeros);
         System.out.println(numerosFiltrados);
 
-    }
-}
+        //-busca
+        //1-for
+        int numeroParaEncontrar = 20;
+        boolean encontrou = false;
+        for(Integer numeroQueprocuro : numeros){
+            if(numeroQueprocuro == numeroParaEncontrar){
+                encontrou = true;
+            }
+        }
+        System.out.println(numeroParaEncontrar);
+        System.out.println("Encontrou? " + numeroParaEncontrar);
+
+        String nomeBuscado = "Clairton";
+        boolean encontrouNome = nomes.contains(nomeBuscado);
+        
+        System.out.println(nomes);
+        System.out.println("Encontrei o nome da pesquisa: " + encontrouNome);
+
+        //-findAny()--procura qualquerCoisa na coleção que eu passar.
+        Optional<Integer> qualquerNumero = numeros.stream()
+        .findAny();
+        System.out.println(qualquerNumero);
+
+        Optional<Integer> primeiroNumero = numeros.stream().findFirst();
+        System.out.println(primeiroNumero);
+
+        Optional<Integer> primeiroPar = numeros.stream()
+        .filter(num -> num % 2 == 0)
+        .findFirst();
+        System.out.println("Encontrei na lista o primeiro número par: " + primeiroPar);
+
+        //MapO método map() em streams Java é usado para transformar cada elemento da coleção original em outro valor, criando uma nova coleção com os resultados. Ele recebe uma função (lambda) que define como cada elemento será transformado.
+        List<Integer> quadrados = numeros.stream()
+        .map(n -> n * n)
+        .collect(Collectors.toList());
+        System.out.println(numeros);
+        System.out.println(quadrados);
+        quadrados.removeIf(n -> n > 200);
+        System.out.println(quadrados);
+
+        //colocar em upcase
+        List<String> nomeMaisculo = nomes.stream()
+        .map(String::toUpperCase)
+        .collect(Collectors.toList());
+        System.out.println(nomeMaisculo);
+
+        //reduce
+        System.out.println("---Usando REDUCE---");
+        int soma = numeros.stream()
+        .reduce(0, (acumulador, numero)-> acumulador + numero);
+        System.out.println("Soma de todos os números: " + soma);
+
+        System.out.println("Concatenando String com reduce");
+        String frase = nomes.stream()
+        .reduce(" ", (concatenador, nome)-> concatenador + " " + nome);
+        System.out.println(frase);
+    }//main
+}//
